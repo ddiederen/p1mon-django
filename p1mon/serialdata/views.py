32240cf41e8ac901181170dbb09774311259c9c7
Dirk Diederen <dirkdiederen@hotmail.com>
2020-05-01 13:26:02 +0200

    remove render_to_response

--- p1mon/serialdata/views.py
+++ p1mon/serialdata/views.py
@@ -1,7 +1,7 @@
 #%% Libraries  
 from django.db.models import Sum
 
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from datetime import datetime, timedelta, date
 
 from constance import config
